eed1f56c4daeebe7063ec42c96f5bcc050eb4445
lethal8723 <5979535+lethal8723@users.noreply.github.com>
2021-03-07 20:30:33 +0000

    Deploying to gh-pages from  @ 426d4d9d2c135ad4d0b726ab46b8dac927386dab 🚀

--- assets/tips/preprocess3/code/ex1-preprocess3.jl
+++ assets/tips/preprocess3/code/ex1-preprocess3.jl
@@ -1,5 +1,7 @@
 # This file was generated, do not modify it. # hide
 using DataFrames,CSV
-path = joinpath("tips","data","reserve.csv")
-reserve_df = CSV.read(path,DataFrame)
-#first(reserve_df) |> println
+path_rsv = joinpath("tips","data","reserve.csv")
+path_hotel = joinpath("tips","data","hotel.csv")
+reserve_df = CSV.read(path_rsv,DataFrame)
+hotel_df = CSV.read(path_hotel,DataFrame)
+#first(hotel_df) |> println
