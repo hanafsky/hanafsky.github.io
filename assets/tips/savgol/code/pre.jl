07e88ef4b3f953785effb1890a7069863852801f
hanafsky <5979535+hanafsky@users.noreply.github.com>
2021-04-04 21:23:47 +0000

    Deploying to gh-pages from  @ 02a8d51cdadc441b4ebb5b0a78271922b434b4ff 🚀

--- assets/tips/savgol/code/pre.jl
+++ assets/tips/savgol/code/pre.jl
@@ -1,6 +1,6 @@
 # This file was generated, do not modify it. # hide
 # hideall
-using Plot,Plots.PlotMeasures
+using Plots,Plots.PlotMeasures
 Plots.reset_defaults()
 default(
     left_margin = 30px,
--- assets/tips/savgol/code/savgol.jl
+++ assets/tips/savgol/code/savgol.jl
@@ -1,6 +1,6 @@
 # This file was generated, do not modify it. # hide
 using SavGol
-sg1 = SavGol.SG(5,2)
+sg1 = SavGol.SG(5,3)
 p0=scatter(sg1[:,1],label="window for smoothing")
 scatter!(sg1[:,2], label="window for 1st order derivative")
 scatter!(sg1[:,3], label="window for 2nd order derivative")
